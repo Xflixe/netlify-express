33cae29ac6f16b114cc2d7b661dd82ccb810020a
Rahul M <rahulreddy93m@gmail.com>
2021-10-28 11:04:39 +0530

    feat: Added demo login btn

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -1,59 +1,74 @@
-import React, { useRef, useState } from 'react'
-import { Form, Button, Card, Alert } from 'react-bootstrap'
-import { useAuth } from '../../contexts/AuthContext'
-import { Link, useHistory } from 'react-router-dom'
-import CenteredContainer from './CenteredContainer'
+import React, { useRef, useState } from "react";
+import { Form, Button, Card, Alert } from "react-bootstrap";
+import { useAuth } from "../../contexts/AuthContext";
+import { Link, useHistory } from "react-router-dom";
+import CenteredContainer from "./CenteredContainer";
 
 export default function Login() {
-  const emailRef = useRef()
-  const passwordRef = useRef()
-  const { login } = useAuth()
-  const [error, setError] = useState('')
-  const [loading, setLoading] = useState(false)
-  const history = useHistory()
+  let emailRef = useRef();
+  let passwordRef = useRef();
+  const { login } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
+
+  console.log(emailRef, passwordRef);
+
+  const handleClick = () => {
+    emailRef.current.value = "test@test.com";
+    passwordRef.current.value = "Test@12345";
+  };
 
   async function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
 
     try {
-      setError('')
-      setLoading(true)
-      await login(emailRef.current.value, passwordRef.current.value)
-      history.push('/')
+      setError("");
+      setLoading(true);
+      await login(emailRef.current.value, passwordRef.current.value);
+      history.push("/");
     } catch {
-      setError('Failed to log in')
+      setError("Failed to log in");
     }
 
-    setLoading(false)
+    setLoading(false);
   }
 
   return (
     <CenteredContainer>
       <Card>
         <Card.Body>
-          <h2 className='text-center mb-4'>Log In</h2>
-          {error && <Alert variant='danger'>{error}</Alert>}
+          <h2 className="text-center mb-4">Log In</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
-            <Form.Group id='email'>
+            <Form.Group id="email">
               <Form.Label>Email</Form.Label>
-              <Form.Control type='email' ref={emailRef} required />
+              <Form.Control type="email" ref={emailRef} required />
             </Form.Group>
-            <Form.Group id='password'>
+            <Form.Group id="password">
               <Form.Label>Password</Form.Label>
-              <Form.Control type='password' ref={passwordRef} required />
+              <Form.Control type="password" ref={passwordRef} required />
             </Form.Group>
-            <Button disabled={loading} className='w-100' type='submit'>
+            <Button disabled={loading} className="w-100" type="submit">
               Login
             </Button>
+            <Button
+              disabled={loading}
+              className="w-100 mt-2"
+              type="submit"
+              onClick={handleClick}
+            >
+              Demo Credentails
+            </Button>
           </Form>
-          <div className='w-100 text-center mt-3'>
-            <Link to='/forgot-password'>Forgot Password?</Link>
+          <div className="w-100 text-center mt-3">
+            <Link to="/forgot-password">Forgot Password?</Link>
           </div>
         </Card.Body>
       </Card>
-      <div className='w-100 text-center mt-2'>
-        Need an account? <Link to='/signup'>Sign Up</Link>
+      <div className="w-100 text-center mt-2">
+        Need an account? <Link to="/signup">Sign Up</Link>
       </div>
     </CenteredContainer>
-  )
+  );
 }
